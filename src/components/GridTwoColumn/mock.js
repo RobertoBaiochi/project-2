4e91db2e46ba5608a88aa88ee7f738d4544f7eee
RobertoBaiochi <robertombbaiochi@gmail.com>
2023-01-05 04:11:35 -0300

    feat: adciona o component gridTwoColumn

---
+++ src/components/GridTwoColumn/mock.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Grid two column',
+  text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, id voluptatum quis autem dolor quod officiis magni totam quasi nam accusamus magnam, eum molestiae perspiciatis rem sapiente in nemo blanditiis?`,
+  srcImg: 'assets/images/javascript.svg',
+};
