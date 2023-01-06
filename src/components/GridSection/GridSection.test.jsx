b9d58f13e1d25af80ca14be368626aff90be0b6f
RobertoBaiochi <robertombbaiochi@gmail.com>
2023-01-05 22:52:03 -0300

    feat: adiciona o componente GridSection

---
+++ src/components/GridSection/GridSection.test.jsx
@@ -0,0 +1,18 @@
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import GridSection from '.';
+import mock from './mock';
+
+describe('<GridSection />', () => {
+  it('should render', () => {
+    const { container } = renderTheme(<GridSection {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render without background', () => {
+    const { container } = renderTheme(
+      <GridSection {...mock} background={undefined} />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/GridSection/mock.js
@@ -0,0 +1,23 @@
+export default {
+  background: false,
+  title: 'My grid',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat totam sequi, iusto sunt vel excepturi?',
+  grid: [
+    {
+      title: 'Teste 1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae repellendus tempora dolore ab. Ullam itaque non alias illum. Provident vel, obcaecati nostrum aliquam fuga perspiciatis eveniet excepturi eius praesentium, dolorum soluta vero maxime quaerat molestiae ipsum, harum ducimus odio?',
+    },
+    {
+      title: 'Teste 2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae repellendus tempora dolore ab. Ullam itaque non alias illum. Provident vel, obcaecati nostrum aliquam fuga perspiciatis eveniet excepturi eius praesentium, dolorum soluta vero maxime quaerat molestiae ipsum, harum ducimus odio?',
+    },
+    {
+      title: 'Teste 3',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio beatae repellendus tempora dolore ab. Ullam itaque non alias illum. Provident vel, obcaecati nostrum aliquam fuga perspiciatis eveniet excepturi eius praesentium, dolorum soluta vero maxime quaerat molestiae ipsum, harum ducimus odio?',
+    },
+  ],
+};
